e60bec9e8a7750e7297a493bbf154ab211ed3723
ben <nguyenvanben02001@gmail.com>
2023-03-26 21:58:16 +0700

    sai password sua thanh username

--- mobileApp/src/screens/NewWallet/index.jsx
+++ mobileApp/src/screens/NewWallet/index.jsx
@@ -18,7 +18,7 @@ function NewWallet({ navigation }) {
 
         <View style={{ alignItems: 'center', marginTop: -20 }}>
           <View style={styles.inputView}>
-            <TextInput style={styles.TextInput} placeholder="Password." placeholderTextColor="#003f5c" secureTextEntry={true} onChangeText={(password) => setPassword(password)} />
+            <TextInput style={styles.TextInput} placeholder="Username." placeholderTextColor="#003f5c" secureTextEntry={true} onChangeText={(password) => setPassword(password)} />
           </View>
           <View style={styles.inputView}>
             <TextInput style={styles.TextInput} placeholder="Password." placeholderTextColor="#003f5c" secureTextEntry={true} onChangeText={(password) => setPassword(password)} />
